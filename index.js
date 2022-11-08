2e098c8997f59e9f182606520f7d7f853d734ccf
sakib-xrz <sakibxrz21@gmail.com>
2022-11-08 13:09:43 +0600

    Get api created

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.08yflhj.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("service-review").collection("services");
+
+    app.get("/services", async(req,res) => {
+      const query = { };
+      const cursor = serviceCollection.find(query).limit(3);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+    app.get("/services/all", async(req,res) => {
+      const query = { };
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+    app.get("/services/all/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
+  } finally {
+    
+  }
+}
+run().catch(err => console.error(err));
+
+
 
 app.get("/", (req, res) => {
   res.send("Service review server is running!!!");
