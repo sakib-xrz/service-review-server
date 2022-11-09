39d8332db246c976b33e9512fd10d63f9da7bfa5
sakib-xrz <sakibxrz21@gmail.com>
2022-11-09 10:59:59 +0600

    single service api create

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+
+     app.get("/services/all/:id", async (req, res) => {
+       const id = req.params.id;
+       const query = { _id: ObjectId(id) };
+       const result = await serviceCollection.findOne(query);
+       res.send(result);
+     });
   } finally {
   }
 }
