fab65d2f915d2387ff9735347029d739f5c36171
sakib-xrz <sakibxrz21@gmail.com>
2022-11-09 10:57:45 +0600

    api update

--- index.js
+++ index.js
@@ -29,19 +29,7 @@ async function run() {
       res.send(services);
     });
 
-    app.get("/services/all", async (req, res) => {
-      const query = {};
-      const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send(services);
-    });
-
-    app.get("/services/all/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await serviceCollection.findOne(query);
-      res.send(result);
-    });
+    
   } finally {
   }
 }
